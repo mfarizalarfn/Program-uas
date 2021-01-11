4892997549d886eb2aa408279c82f916e555fd84
Mohamad Farizal Arifin <farizalarifin4777@gmail.com>
2021-01-12 01:25:33 +0700

    update python file

--- model/daftar_nilai.py
+++ model/daftar_nilai.py
@@ -1,48 +1,27 @@
 from prettytable import PrettyTable
 
-print("=======================================")
-print("Nama : Mohamad Farizal Arifin")
-print("NIM : 312010231")
-print("Kelas : TI.20.B.1")
-print("Mata Kuliah : Bahasa Pemrograman")
-print("Ujian Akhir Semester 1")
-print("=======================================")
+# Fungsi : tambah_data, ubah_data, hapus_data, cari_data
 
-tampunglist = {}
 x = PrettyTable()
+tampunglist = {}
 
 
-def tambah():
+def tambah_data():
     print("========== TAMBAH DATA NILAI MAHASISWA ==========")
     tnama = input("Masukkan Nama Mahasiswa : ")
     tnim = input("Masukkan NIM Mahasiswa : ")
-    ttugas = int(input("Masukkan Nilai Tugas Mahasiswa : "))
-    tuts = int(input("Masukkan Nilai UTS Mahasiswa : "))
-    tuas = int(input("Masukkan Nilai UAS Mahasiswa : "))
-    takhir = 0.3 * float(ttugas) + 0.35 * float(tuts) + 0.35 * float(tuas)
-    tampunglist[tnama] = tnim, ttugas, tuts, tuas, takhir
+    tampunglist[tnama] = tnim
 
 
-def tampilkan():
-    print("========== LIHAT DATA NILAI MAHASISWA ==========")
-    no = 0
-    x.field_names = ["NO", "NAMA", " NIM", "TUGAS", "UTS", "UAS", "AKHIR"]
-    for tdata in tampunglist.items():
-        no += 1
-        x.add_row([no, tdata[0], tdata[1][0], tdata[1][1], tdata[1][2], tdata[1][3], tdata[1][4]])
-    print(x)
-
-
-def hapus(hxsiapa):
+def hapus_data():
     print("========== HAPUS DATA NILAI MAHASISWA ==========")
-    if hxsiapa in tampunglist.keys():
-        print(f"DATA {hxsiapa} BERHASIL DIHAPUS")
-        del tampunglist[hxsiapa]
-    else:
-        print("!!! === ERROR! DATA TIDAK TERSEDIA === !!!")
 
 
-def ubah(xsiapa):
+def cari_data():
+    print("========== CARI DATA NILAI MAHASISWA ==========")
+
+
+def ubah_data(xsiapa):
     if xsiapa in tampunglist.keys():
         print("Data apa yang akan diubah ? : ")
         mhs = int(input(" 1. NIM \n 2. Nilai Tugas \n 3. Nilai UTS \n 4. Nilai UAS\n Pilih dengan angka (1/2/3/4) : "))
@@ -56,7 +35,7 @@ def ubah(xsiapa):
             tampunglist[xsiapa] = ubahnim, vtug, vuts, vuas, vakh
             x.field_names = ["No", "NAMA", " NIM", "TUGAS", "UTS", "UAS", "AKHIR"]
             for tdata in tampunglist.items():
-                i += 1
+                i += 2
                 x.add_row([i, tdata[0], tdata[1][0], tdata[1][1], tdata[1][2], tdata[1][3], tdata[1][4]])
             print(x)
         elif mhs == 2:
@@ -69,7 +48,7 @@ def ubah(xsiapa):
             tampunglist[xsiapa] = vnim, ubahtugas, vuts, vuas, vakh
             x.field_names = ["No", "NAMA", " NIM", "TUGAS", "UTS", "UAS", "AKHIR"]
             for tdata in tampunglist.items():
-                i += 1
+                i += 2
                 x.add_row([i, tdata[0], tdata[1][0], tdata[1][1], tdata[1][2], tdata[1][3], tdata[1][4]])
             print(x)
         elif mhs == 3:
@@ -82,7 +61,7 @@ def ubah(xsiapa):
             tampunglist[xsiapa] = vnim, vtug, ubahuts, vuas, vakh
             x.field_names = ["No", "NAMA", " NIM", "TUGAS", "UTS", "UAS", "AKHIR"]
             for tdata in tampunglist.items():
-                i += 1
+                i += 2
                 x.add_row([i, tdata[0], tdata[1][0], tdata[1][1], tdata[1][2], tdata[1][3], tdata[1][4]])
             print(x)
         elif mhs == 4:
@@ -95,34 +74,10 @@ def ubah(xsiapa):
             tampunglist[xsiapa] = vnim, vtug, vuts, ubahuas, vakh
             x.field_names = ["No", "NAMA", " NIM", "TUGAS", "UTS", "UAS", "AKHIR"]
             for tdata in tampunglist.items():
-                i += 1
+                i += 2
                 x.add_row([i, tdata[0], tdata[1][0], tdata[1][1], tdata[1][2], tdata[1][3], tdata[1][4]])
             print(x)
         else:
             print("!!! === ERROR! Anda Memasukkan Pilihan yang Salah === !!!")
     else:
         print("!!! === ERROR! DATA TIDAK TERSEDIA === !!!")
-
-
-print("===== APLIKASI PENGOLAHAN DATA NILAI MAHASISWA =====")
-while True:
-    print("MENU : \n 1. Tambah Data \n 2. Lihat Data \n 3. Ubah Data \n 4. Hapus Data \n 5. Keluar Aplikasi")
-    pilih = int(input("Pilih Menu (1/2/3/4/5) : "))
-    if pilih == 1:
-        tambah()
-    elif pilih == 2:
-        tampilkan()
-    elif pilih == 3:
-        print("========== UBAH DATA NILAI MAHASISWA ==========")
-        print("Data siapa yang akan diubah ?")
-        siapa = input("Masukkan Nama Mahasiswa yang akan diubah : ")
-        ubah(siapa)
-    elif pilih == 4:
-        print("========== HAPUS DATA NILAI MAHASISWA ==========")
-        print("Data siapa yang akan diubah ?")
-        hsiapa = input("Masukkan Nama Mahasiswa yang akan diubah : ")
-        hapus(hsiapa)
-    elif pilih == 5:
-        break
-    else:
-        print("!!! === ERROR! Pilihan Yang Anda Masukan Salah === !!!")
