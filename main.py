7214cddc8212408b476000251b9a87a1b87d199d
Mohamad Farizal Arifin <farizalarifin4777@gmail.com>
2021-01-11 22:18:29 +0700

    update python file

---
+++ main.py
@@ -0,0 +1,128 @@
+from prettytable import PrettyTable
+
+print("=======================================")
+print("Nama : FEBRO HERDYANTO")
+print("NIM : 312010043")
+print("Kelas : TI.20.B.1")
+print("Mata Kuliah : Bahasa Pemrograman")
+print("Tugas : Praktikum Lab 6 - Pertemuan 10")
+print("=======================================")
+
+tampunglist = {}
+x = PrettyTable()
+
+
+def tambah():
+    print("========== TAMBAH DATA NILAI MAHASISWA ==========")
+    tnama = input("Masukkan Nama Mahasiswa : ")
+    tnim = input("Masukkan NIM Mahasiswa : ")
+    ttugas = int(input("Masukkan Nilai Tugas Mahasiswa : "))
+    tuts = int(input("Masukkan Nilai UTS Mahasiswa : "))
+    tuas = int(input("Masukkan Nilai UAS Mahasiswa : "))
+    takhir = 0.3 * float(ttugas) + 0.35 * float(tuts) + 0.35 * float(tuas)
+    tampunglist[tnama] = tnim, ttugas, tuts, tuas, takhir
+
+
+def tampilkan():
+    print("========== LIHAT DATA NILAI MAHASISWA ==========")
+    no = 0
+    x.field_names = ["NO", "NAMA", " NIM", "TUGAS", "UTS", "UAS", "AKHIR"]
+    for tdata in tampunglist.items():
+        no += 1
+        x.add_row([no, tdata[0], tdata[1][0], tdata[1][1], tdata[1][2], tdata[1][3], tdata[1][4]])
+    print(x)
+
+
+def hapus(hxsiapa):
+    print("========== HAPUS DATA NILAI MAHASISWA ==========")
+    if hxsiapa in tampunglist.keys():
+        print(f"DATA {hxsiapa} BERHASIL DIHAPUS")
+        del tampunglist[hxsiapa]
+    else:
+        print("!!! === ERROR! DATA TIDAK TERSEDIA === !!!")
+
+
+def ubah(xsiapa):
+    if xsiapa in tampunglist.keys():
+        print("Data apa yang akan diubah ? : ")
+        mhs = int(input(" 1. NIM \n 2. Nilai Tugas \n 3. Nilai UTS \n 4. Nilai UAS\n Pilih dengan angka (1/2/3/4) : "))
+        if mhs == 1:
+            ubahnim = input("Silahkan masukan NIM yang benar : ")
+            i = 0
+            vtug = tampunglist[xsiapa][1]
+            vuts = tampunglist[xsiapa][2]
+            vuas = tampunglist[xsiapa][3]
+            vakh = tampunglist[xsiapa][4]
+            tampunglist[xsiapa] = ubahnim, vtug, vuts, vuas, vakh
+            x.field_names = ["No", "NAMA", " NIM", "TUGAS", "UTS", "UAS", "AKHIR"]
+            for tdata in tampunglist.items():
+                i += 1
+                x.add_row([i, tdata[0], tdata[1][0], tdata[1][1], tdata[1][2], tdata[1][3], tdata[1][4]])
+            print(x)
+        elif mhs == 2:
+            ubahtugas = int(input("Masukkan Nilai Tugas yang benar : "))
+            i = 0
+            vnim = tampunglist[xsiapa][0]
+            vuts = tampunglist[xsiapa][2]
+            vuas = tampunglist[xsiapa][3]
+            vakh = tampunglist[xsiapa][4]
+            tampunglist[xsiapa] = vnim, ubahtugas, vuts, vuas, vakh
+            x.field_names = ["No", "NAMA", " NIM", "TUGAS", "UTS", "UAS", "AKHIR"]
+            for tdata in tampunglist.items():
+                i += 1
+                x.add_row([i, tdata[0], tdata[1][0], tdata[1][1], tdata[1][2], tdata[1][3], tdata[1][4]])
+            print(x)
+        elif mhs == 3:
+            ubahuts = int(input("Masukkan Nilai UTS yang benar : "))
+            i = 0
+            vnim = tampunglist[xsiapa][0]
+            vtug = tampunglist[xsiapa][1]
+            vuas = tampunglist[xsiapa][3]
+            vakh = tampunglist[xsiapa][4]
+            tampunglist[xsiapa] = vnim, vtug, ubahuts, vuas, vakh
+            x.field_names = ["No", "NAMA", " NIM", "TUGAS", "UTS", "UAS", "AKHIR"]
+            for tdata in tampunglist.items():
+                i += 1
+                x.add_row([i, tdata[0], tdata[1][0], tdata[1][1], tdata[1][2], tdata[1][3], tdata[1][4]])
+            print(x)
+        elif mhs == 4:
+            ubahuas = int(input("Masukkan Nilai UAS yang benar : "))
+            i = 0
+            vnim = tampunglist[xsiapa][0]
+            vtug = tampunglist[xsiapa][1]
+            vuts = tampunglist[xsiapa][2]
+            vakh = tampunglist[xsiapa][4]
+            tampunglist[xsiapa] = vnim, vtug, vuts, ubahuas, vakh
+            x.field_names = ["No", "NAMA", " NIM", "TUGAS", "UTS", "UAS", "AKHIR"]
+            for tdata in tampunglist.items():
+                i += 1
+                x.add_row([i, tdata[0], tdata[1][0], tdata[1][1], tdata[1][2], tdata[1][3], tdata[1][4]])
+            print(x)
+        else:
+            print("!!! === ERROR! Anda Memasukkan Pilihan yang Salah === !!!")
+    else:
+        print("!!! === ERROR! DATA TIDAK TERSEDIA === !!!")
+
+
+print("===== APLIKASI PENGOLAHAN DATA NILAI MAHASISWA =====")
+while True:
+    print("MENU : \n 1. Tambah Data \n 2. Lihat Data \n 3. Ubah Data \n 4. Hapus Data \n 5. Keluar Aplikasi")
+    pilih = int(input("Pilih Menu (1/2/3/4/5) : "))
+    if pilih == 1:
+        tambah()
+    elif pilih == 2:
+        tampilkan()
+    elif pilih == 3:
+        print("========== UBAH DATA NILAI MAHASISWA ==========")
+        print("Data siapa yang akan diubah ?")
+        siapa = input("Masukkan Nama Mahasiswa yang akan diubah : ")
+        ubah(siapa)
+    elif pilih == 4:
+        print("========== HAPUS DATA NILAI MAHASISWA ==========")
+        print("Data siapa yang akan diubah ?")
+        hsiapa = input("Masukkan Nama Mahasiswa yang akan diubah : ")
+        hapus(hsiapa)
+    elif pilih == 5:
+        break
+    else:
+        print("!!! === ERROR! Anda Memasukkan Pilihan yang Salah === !!!")
